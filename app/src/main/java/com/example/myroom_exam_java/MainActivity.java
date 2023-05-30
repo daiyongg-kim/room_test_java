f6fdf25f6cf661e41ef592896e4ebbf749eab5ca
Daiyong Kim <daiyongg.kim@gmail.com>
2023-05-29 22:29:52 -0700

    update with databinding

--- app/src/main/java/com/example/myroom_exam_java/MainActivity.java
+++ app/src/main/java/com/example/myroom_exam_java/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myroom_exam_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.loader.content.AsyncTaskLoader;
@@ -12,39 +14,35 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.myroom_exam_java.databinding.ActivityMainBinding;
+
 import java.nio.channels.AsynchronousChannelGroup;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mTodoEditText;
-    private TextView mResultTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTodoEditText = findViewById(R.id.todo_edit);
-        mResultTextView = findViewById(R.id.result_text);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
         MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
+        binding.setViewModel(viewModel);
         //UI refresh
-        viewModel.getAll().observe(this, todos -> {
-            mResultTextView.setText(todos.toString());
-        });
+//        viewModel.getAll().observe(this, todos -> {
+//            binding.resultText.setText(todos.toString());
+//        });
 
         // DB insert when the add button clicked
-        findViewById(R.id.add_button).setOnClickListener(v -> {
-            //db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-            viewModel.insert(new Todo(mTodoEditText.getText().toString()));
-        });
+//        findViewById(R.id.add_button).setOnClickListener(v -> {
+//            viewModel.insert(new Todo(binding.todoEdit.getText().toString()));
+//        });
 
         // Delete all when the reset button clicked
-        findViewById(R.id.remove_button).setOnClickListener(v ->{
-//                db.todoDao().deleteAll();
-            viewModel.reset(new Todo());
-        });
+//        findViewById(R.id.remove_button).setOnClickListener(v ->{
+//            viewModel.reset();
+//        });
     }
 }
